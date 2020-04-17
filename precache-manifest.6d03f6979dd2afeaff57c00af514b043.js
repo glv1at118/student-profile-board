45ff900cb8ec5cf9fdbaaddc2f14d43640c3cbbb
Guannan <guannanlv@gmail.com>
2020-04-17 17:27:47 -0400

    Updates

---
+++ precache-manifest.6d03f6979dd2afeaff57c00af514b043.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba38ac0b4ed10303d3ca5f13f11be5fe",
+    "url": "/student-profile-board/index.html"
+  },
+  {
+    "revision": "8d7fa4f6aa58b91fe463",
+    "url": "/student-profile-board/static/css/main.f93642f5.chunk.css"
+  },
+  {
+    "revision": "32fe80c5a204b1535b8a",
+    "url": "/student-profile-board/static/js/2.4603bdb3.chunk.js"
+  },
+  {
+    "revision": "8d7fa4f6aa58b91fe463",
+    "url": "/student-profile-board/static/js/main.4abf95e3.chunk.js"
+  },
+  {
+    "revision": "b2071ea0338a0c4c9aa8",
+    "url": "/student-profile-board/static/js/runtime-main.85f172d6.js"
+  },
+  {
+    "revision": "47f2876726abf2fe5580782d1b8e72df",
+    "url": "/student-profile-board/static/media/icomoon.47f28767.ttf"
+  },
+  {
+    "revision": "80a1aaf2d9f6320a7b855f2b197a7618",
+    "url": "/student-profile-board/static/media/icomoon.80a1aaf2.svg"
+  },
+  {
+    "revision": "8b7fb11072faf74ca2a2ad08bfa94520",
+    "url": "/student-profile-board/static/media/icomoon.8b7fb110.eot"
+  },
+  {
+    "revision": "fa26232289901e24c3b5819e8b53d017",
+    "url": "/student-profile-board/static/media/icomoon.fa262322.woff"
+  }
+]);
